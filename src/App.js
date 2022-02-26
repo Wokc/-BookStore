4e1952244476a39f5d7195d6da2f5f1ab00c7d37
Wokc <mozgomozgo93@mail.ru>
2022-02-26 12:50:19 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <header className="header">
+          <div className="header__logo"><a href='#'>Books</a></div>
+              <ul>
+                  <li>Biographies</li>
+                  <li>Business</li>
+                  <li>Education</li>
+                  <li>History</li>
+                  <li>Children's</li>
+                  <li>Classic</li>
+              </ul>
+        </header>
+        <div className="sidebar">
+            <div className="sidebar__card"><a href="#"></a></div>
+            <div className="sidebar__search"><input type="text" placeholder="Search" /></div>
+            <div className="sidebar__profile"></div>
+            <div className="sidebar__options"></div>
+        </div>
+        <div className="catalog">
+              <div className="catalog__products">
+                    <div className="catalog__header">
+                        <h2>New books</h2>
+                    </div>
+              </div>
+        </div>
     </div>
   );
 }
